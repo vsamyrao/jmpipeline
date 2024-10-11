a43ae170104a53a460f32addf576e47ca8e2f465
vsamyrao <65747285+vsamyrao@users.noreply.github.com>
2024-10-11 18:59:09 -0400

    Update performance-test.js

--- loadtests/performance-test.js
+++ loadtests/performance-test.js
@@ -1,6 +1,9 @@
 import { sleep } from"k6";
 import http from "k6/http";
 
+import { jUnit } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
+
+
 export let options = {
   duration: "1m",
   vus: 50,
@@ -13,3 +16,11 @@ export default function() {
   http.get("http://test.k6.io/contacts.php");
   sleep(3);
 };
+
+export function handleSummary(data) {
+  console.log('Preparing the end-of-test summary...');
+
+  return {
+    'junit.xml': jUnit(data), // Transform summary and save it as a JUnit XML...
+  };
+}
